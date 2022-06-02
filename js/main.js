a5b2ac0ef01cdc6d39f6282e6f87a4f1444c3b62
Arturo <contreraschaparroarturo@gmail.com>
2022-06-02 13:56:21 -0600

    Se agrego validaciones y accion al boton

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-
-
+let contador=0;
+let costoTotal=0;
 let element = document.getElementById("totalPrecio");
 element.innerHTML="Total en precio";
 
@@ -7,7 +7,7 @@ let txtNombre = document.getElementById("Name");
 //txtNombre.value="leche semidescremada";
 //console.log(txtNombre.value);
 let txtNumber= document.getElementById("Number");
-
+let total= document.getElementById("precioTotal");
 /* let campos=document.getElementsByClassName("campo");
 campos[0].value  = "Leche descremada deslactosada light=Agua";
 console.log(campos[0].value);
@@ -31,14 +31,61 @@ let cuerpoTabla = tabla.getElementsByTagName("tbody");
 <td>1</td>
 <td>$23</td>
 </tr>`; */
+function validarNombre(){
+    if (txtNombre.value.length <3){
+        return false
+    }//if
+    return true;
+}// ValidarNombre
 
-
+function validarCantidad() {
+    if(txtNumber.value.length==0){
+    return false;
+}//if
+    if (isNaN(txtNumber.value)){
+        return false;
+    }//if
+    if (parseFloat(txtNumber.value)<=0){
+        return false;
+    }//if
+    return true;
+}//validarCantidad
 let agregar = document.getElementById("btnAgregar");
                                 //event en este caso es una variable, puedes ponerle cualquier(x, y, perro, etc) nombre pero la mayoria utiliza el "event"
-agregar.addEventListener("click",(event)=>{
-    let precio = Math.random() * 50;
+  agregar.addEventListener("click",(event)=>{
+    event.preventDefault();
+    if((! validarNombre()) || (! validarCantidad())  ){
+        let lista="";
+        if(!validarNombre()){
+            txtNombre.style.border="red thin solid";
+            lista+="<li>Se debe escribir un nombre valido</li>";
+        }//
+        if(!validarCantidad()){
+            txtNumber.style.border="red thin solid";
+            lista+="<li>Se debe escribir una cantidad valida</li>";
+        }//
+        document.getElementById("alertValidacionesTexto").innerHTML=
+        `Los campos deben ser llenados correctamente. 
+        <ul>${lista}</ul>`;
+        document.getElementById("alertValidaciones").style.display="block";
+        setTimeout(function(){
+            document.getElementById("alertValidaciones").style.display="none";
+        },
+        8000
+        );
+        return false;
+    }//if
+    txtNumber.style.border="";
+    txtNombre.style.border="";
+    document.getElementById("alertValidaciones").style.display="none";
+    contador++;
+    document.getElementById("contadorProductos").innerHTML=contador;
+    let precio =(Math.floor((Math.random() * 50)*100))/100;
+    let cantidad = parseFloat(txtNumber.value);
+    costoTotal += (precio * cantidad);
+    total.innerHTML =`$ ${Math.floor(costoTotal*100)/100}`
     let tmp = `<tr>
-<th scope="row">1</th>
+<th scope="row">${contador}</th>
 <td>${txtNombre.value}</td>
 <td>${txtNumber.value}</td>
 <td>$ ${precio}</td>
@@ -49,8 +96,17 @@ txtNumber.value="";
 txtNombre.value="";
 txtNombre.focus();
 });
-
-
-
-//agregar.onclick - no es la recomendada
+                         //blur hace efecto cuando el elemento pierde el focus
+txtNombre.addEventListener("blur", (event)=> {
+    event.target.value=event.target.value.trim();
+}
+);
+txtNumber.addEventListener("blur", (event)=> {
+    event.target.value=event.target.value.trim();});
+    /*trim quita el espacio en blanco de ambos extremos de una cadena y
+     devuelve una nueva cadena, sin modificar la cadena original. El 
+     espacio en blanco en este contexto son todos los caracteres de espacio
+      en blanco (espacio, tabulación, espacio sin interrupción, etc.) 
+      y todos los caracteres de terminación de línea (LF, CR, etc.).*/
+//target es el objetivo del evento
 
